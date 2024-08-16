394fda8ed69557db26507362987fcb1528942474
Marek Zelinka <mzelinka17@gmail.com>
2024-08-16 15:40:06 +0200

    solve ex 9.15

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
+import type { CoursePart } from './types';
+import assertNever from './utils';
+
 const App = () => {
   const courseName = 'Half Stack application development';
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
       exerciseCount: 10,
+      description: 'This is an awesome course part',
+      kind: 'basic',
     },
     {
       name: 'Using props to pass data',
       exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: 'group',
+    },
+    {
+      name: 'Basics of type Narrowing',
+      exerciseCount: 7,
+      description: 'How to go from unknown to string',
+      kind: 'basic',
     },
     {
       name: 'Deeper type usage',
       exerciseCount: 14,
+      description: 'Confusing description',
+      backgroundMaterial:
+        'https://type-level-typescript.com/template-literal-types',
+      kind: 'background',
+    },
+    {
+      name: 'TypeScript in frontend',
+      exerciseCount: 10,
+      description: 'a hard part',
+      kind: 'basic',
+    },
+    {
+      name: 'Backend development',
+      exerciseCount: 21,
+      description: 'Typing the backend',
+      requirements: ['nodejs', 'jest'],
+      kind: 'special',
     },
   ];
 
@@ -35,20 +65,78 @@ const Header = ({ name }: { name: string }) => {
   return <h1>{name}</h1>;
 };
 
-const Content = ({
-  parts,
-}: {
-  parts: { name: string; exerciseCount: number }[];
-}) => {
+const Content = ({ parts }: { parts: CoursePart[] }) => {
   return (
     <div>
       {parts.map((part) => (
-        <p key={part.name}>
-          {part.name} {part.exerciseCount}
-        </p>
+        <PartItem key={part.name} part={part} />
       ))}
     </div>
   );
+};
+
+const PartItem = ({ part }: { part: CoursePart }) => {
+  switch (part.kind) {
+    case 'basic': {
+      return (
+        <div>
+          <div>
+            <strong>
+              {part.name} {part.exerciseCount}
+            </strong>
+          </div>
+          <div>
+            <em>{part.description}</em>
+          </div>
+        </div>
+      );
+    }
+    case 'group': {
+      return (
+        <div>
+          <div>
+            <strong>
+              {part.name} {part.exerciseCount}
+            </strong>
+          </div>
+          <div>project exercises {part.groupProjectCount}</div>
+        </div>
+      );
+    }
+    case 'background': {
+      return (
+        <div>
+          <div>
+            <strong>
+              {part.name} {part.exerciseCount}
+            </strong>
+          </div>
+          <div>
+            <em>{part.description}</em>
+          </div>
+          <div>submit to {part.backgroundMaterial}</div>
+        </div>
+      );
+    }
+    case 'special': {
+      return (
+        <div>
+          <div>
+            <strong>
+              {part.name} {part.exerciseCount}
+            </strong>
+          </div>
+          <div>
+            <em>{part.description}</em>
+          </div>
+          <div>required skils: {part.requirements.join(', ')}</div>
+        </div>
+      );
+    }
+    default: {
+      return assertNever(part);
+    }
+  }
 };
 
 const Total = ({ totalExercises }: { totalExercises: number }) => {
